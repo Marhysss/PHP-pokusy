1b7a812b0259805c693e686ef2dd6ee3acbd6fed
Marhysss <marhantomas236@student.voskh.cz>
2025-01-15 22:20:46 +0100

    php

---
+++ edit_task.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+require 'includes/db_connect.php';
+
---
+++ includes/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+$connection = mysqli_connect("localhost", "root", "", "loginapp");
+
+if (!$connection) {
+    die("ses asi kkt " . mysqli_connect_error());
+} else {
+    echo "Hotovsom";
+}
+?>
