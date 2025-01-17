f86d4a7693e2abc2081e56511b804e3d773aca29
Marhysss <marhantomas236@student.voskh.cz>
2025-01-17 19:07:47 +0100

    lehká úprava db connect

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -3,7 +3,6 @@ $connection = mysqli_connect("localhost", "root", "", "loginapp");
 
 if (!$connection) {
     die("ses asi kkt " . mysqli_connect_error());
-} else {
-    echo "Hotovsom";
-}
+} else 
+    
 ?>
